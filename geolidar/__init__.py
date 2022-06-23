a647908ec5a9720d252849ed2ec210e0dcaf7443
tutorialcreation <tutorialcreation81@gmail.com>
2022-06-23 10:15:57 +0300

    completed writing visualization script

--- geolidar/__init__.py
+++ geolidar/__init__.py
@@ -3,3 +3,4 @@ from .runner import runner
 from .fetch_metadata import fetch
 from .constants import state_mapper
 from .visualizations import visualize
+from .standardizer import standardize
---
+++ geolidar/standardizer.py
@@ -0,0 +1,12 @@
+import numpy as np
+import geopandas as gpd
+
+
+class Standardizer(object):
+
+    def __init__(self) -> None:
+        pass
+
+    
+
+standardize = Standardizer()
